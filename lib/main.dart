f2b1a3afa6fd9de5df3dc0103d59a264496eaac7
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-05-08 08:41:52 -0400

    feat: right tittle  on main

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Yes No App',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Material App Bar'),
+          title: const Text('Yes No App'),
         ),
         body: const Center(
           child: Text('Hello World'),
